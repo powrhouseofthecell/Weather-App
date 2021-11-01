afbaac5b63d5795ef312f9bc940b63c2b9a7adfa
Zuhaib <theshahzuhaib96@gmail.com>
2021-11-01 20:41:54 +0530

    Add final files

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 import getWeather from './utils/open-weather.js';
 import dotenv from 'dotenv';
 dotenv.config();
-
 const cityName = process.argv[2];
-
 if (!cityName) console.log('Please provide a city name!');
 else {
    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.API_KEY}`;
