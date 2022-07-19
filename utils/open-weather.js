5fcc340fc100a92d49b40b9ddfedd89673f30ff4
Zuhaib <theshahzuhaib96@gmail.com>
2022-07-19 15:04:20 +0530

    prettier

--- utils/open-weather.js
+++ utils/open-weather.js
@@ -1,17 +1,17 @@
 // const axios = require('axios');
 import axios from 'axios';
 
-const getWeather = (url) => {
+const getWeather = url => {
    axios
       .get(url)
-      .then((response) => {
+      .then(response => {
          // automatically parses json
          const tmp = Math.round(response.data.main.temp - 273.15);
          console.log(
             `temp: ${tmp}, Weather: ${response.data.weather[0].description}, Humidity: ${response.data.main.humidity}, Clouds: ${response.data.clouds.all}, Country: ${response.data.sys.country}`
          );
       })
-      .catch((err) => {
+      .catch(err => {
          if (err.response) {
             console.log(
                `${err.response.data.cod}, ${err.response.data.message}`
